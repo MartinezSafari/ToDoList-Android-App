782068273fac2d794d0c3943957652038b42d89b
martinez <martinez@thereachtrust.org>
2022-12-31 00:27:30 +0200

    add new items activity + layout

---
+++ app/src/main/java/org/thereachtrust/todolist/ItemsActivity.kt
@@ -0,0 +1,11 @@
+package org.thereachtrust.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.items)
+    }
+}
