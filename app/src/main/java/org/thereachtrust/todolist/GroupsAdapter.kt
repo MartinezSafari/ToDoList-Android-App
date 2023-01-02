2d79c645a0e46d45d5c82099d1cd46a032705ec0
martinez <martinez@thereachtrust.org>
2023-01-02 14:45:18 +0200

    Group Click Listener

--- app/src/main/java/org/thereachtrust/todolist/GroupsAdapter.kt
+++ app/src/main/java/org/thereachtrust/todolist/GroupsAdapter.kt
@@ -4,7 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class GroupsAdapter (private val list: List<Group>): RecyclerView.Adapter<GroupsViewHolder>() {
+class GroupsAdapter (private val list: List<Group>, listenerContext: OnGroupClickListener):
+    RecyclerView.Adapter<GroupsViewHolder>()
+{
+
+    private var myInterface: OnGroupClickListener = listenerContext
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupsViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return GroupsViewHolder(inflater, parent)
@@ -13,6 +18,15 @@ class GroupsAdapter (private val list: List<Group>): RecyclerView.Adapter<Groups
     override fun onBindViewHolder(holder: GroupsViewHolder, position: Int) {
         val group = list[position]
         holder.bind(group)
+
+        holder.itemView.setOnClickListener{
+            myInterface.groupClicked(position)
+        }
+
+        holder.itemView.setOnLongClickListener {
+            myInterface.groupLongClicked(position)
+            true
+        }
     }
 
     override fun getItemCount(): Int = list.size
